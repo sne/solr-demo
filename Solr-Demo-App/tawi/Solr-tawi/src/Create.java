cfaae604f7d409b181d9a1c65db39f89a0e4c0c1
Tamara <ttwidmer@ussers.noreply.github.com>
2015-06-18 13:58:47 +0200

    SolrJ jar files and Create.java

---
+++ Solr-Demo-App/tawi/Solr-tawi/src/Create.java
@@ -0,0 +1,25 @@
+	import java.io.IOException;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.common.SolrInputDocument;
+public class Create {
+	
+	public static void main(String[] args) throws SolrServerException, IOException{
+		System.out.println("bla");
+		
+		SolrClient server = new HttpSolrClient("http://localhost:8983/solr/tawi_core");
+	    for(int i=0;i<1000000;++i) {
+	      SolrInputDocument doc = new SolrInputDocument();
+	      doc.addField("cat", "book");
+	      doc.addField("id", "book-" + i);
+	      doc.addField("name", "SNB History " + i);
+	      server.add(doc);
+	    }
+	    server.commit();
+	  }
+		
+	}
+
+
